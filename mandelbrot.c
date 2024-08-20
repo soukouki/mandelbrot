da155aee4417ac516d4894fc00f39795616b887a
soukouki <soukouki0@yahoo.co.jp>
2024-08-21 01:52:11 +0900

    1ドットの大きさを計算し直すようにして、前よりもすこしズームできるようにした。これ以上は多倍長整数を実装しないといけない

--- mandelbrot.c
+++ mandelbrot.c
@@ -11,6 +11,12 @@ fixed_point fp_from_int(int n) {
     return n * 10000;
 }
 
+// 固定小数点数を初期化
+// 10000倍の値を受け取る(暫定的な運用、後で消す)
+fixed_point fp_from_int10000(int n) {
+    return n;
+}
+
 // 整数に変換
 int fp_to_int(fixed_point x) {
     return x / 10000;
@@ -59,23 +65,28 @@ fixed_point re_min;
 fixed_point re_max;
 fixed_point im_min;
 fixed_point im_max;
-fixed_point d_width;
-fixed_point d_height;
 
 int draw() {
+    // 頻繁に使う値は事前に計算しておく
+    fixed_point fp0 = fp_from_int(0);
+    fixed_point fp2 = fp_from_int(2);
+    fixed_point fp4 = fp_from_int(4);
+    fixed_point re_size = fp_sub(re_max, re_min);
+    fixed_point im_size = fp_sub(im_max, im_min);
     for (int y = 0; y < char_height; y++) {
         for (int x = 0; x < char_width; x++) {
             // 実部と虚部を計算
-            fixed_point re = fp_add(re_min, fp_mul(fp_from_int(x), d_width));
-            fixed_point im = fp_add(im_min, fp_mul(fp_from_int(y), d_height));
+            fixed_point re = fp_add(re_min, fp_mul(re_size, fp_from_int10000(x * 100 / char_width * 100)));
+            fixed_point im = fp_add(im_min, fp_mul(im_size, fp_from_int10000(y * 100 / char_height * 100)));
+            // printf("%d %d\n", re, im);
 
-            fixed_point zx = fp_from_int(0);
-            fixed_point zy = fp_from_int(0);
+            fixed_point zx = fp0;
+            fixed_point zy = fp0;
             int iter = 0;
 
-            while (fp_add(fp_mul(zx, zx), fp_mul(zy, zy)) < fp_from_int(4) && iter < max_iter) {
+            while (fp_add(fp_mul(zx, zx), fp_mul(zy, zy)) < fp4 && iter < max_iter) {
                 fixed_point temp = fp_add(fp_sub(fp_mul(zx, zx), fp_mul(zy, zy)), re);
-                zy = fp_add(fp_mul(fp_mul(fp_from_int(2), zx), zy), im);
+                zy = fp_add(fp_mul(fp_mul(fp2, zx), zy), im);
                 zx = temp;
                 iter++;
             }
@@ -148,16 +159,12 @@ int operate() {
         re_max = fp_div(fp_add(re_max, re_center), fp_from_int(2));
         im_min = fp_div(fp_add(im_min, im_center), fp_from_int(2));
         im_max = fp_div(fp_add(im_max, im_center), fp_from_int(2));
-        d_width = fp_div(d_width, fp_from_int(2));
-        d_height = fp_div(d_height, fp_from_int(2));
     } else if(c == 111) { // 'o'
         // 2倍に縮小
         re_min = fp_add(re_min, fp_sub(re_min, re_center));
         re_max = fp_add(re_max, fp_sub(re_max, re_center));
         im_min = fp_add(im_min, fp_sub(im_min, im_center));
         im_max = fp_add(im_max, fp_sub(im_max, im_center));
-        d_width = fp_mul(d_width, fp_from_int(2));
-        d_height = fp_mul(d_height, fp_from_int(2));
     } else if(c == 107) { // 'k'
         // max_iterを増やす
         max_iter = max_iter * 2;
@@ -172,24 +179,20 @@ int main() {
     // 画面サイズ
     char_width = 200;
     char_height = 70;
-    // char_width = 100;
-    // char_height = 40;
+    // char_width = 40;
+    // char_height = 20;
     max_iter = 1000;
     // 描画範囲の設定 (-1.5, -1)から(0.5, 1)
     re_min = fp_div(fp_from_int(-15), fp_from_int(10));
     re_max = fp_div(fp_from_int(5), fp_from_int(10));
     im_min = fp_from_int(-1);
     im_max = fp_from_int(1);
-    // 描画範囲を幅と高さで割る
-    d_width = fp_div(fp_sub(re_max, re_min), fp_from_int(char_width));
-    d_height = fp_div(fp_sub(im_max, im_min), fp_from_int(char_height));
 
     int stop = 0;
     while(stop == 0) {
         printf("%c[2J", 27);
         draw();
-        printf("re_min: %d, re_max: %d, im_min: %d, im_max: %d, ", re_min, re_max, im_min, im_max);
-        printf("d_width: %d, d_height: %d, max_iter: %d\n", d_width, d_height, max_iter);
+        printf("re_min: %d, re_max: %d, im_min: %d, im_max: %d, max_iter: %d\n", re_min, re_max, im_min, im_max, max_iter);
         stop = operate();
     }
     return 0;
